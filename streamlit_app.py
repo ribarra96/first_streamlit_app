f2d826a0052808cc107d2901fb4de469a3d099cf
ribarra96 <ricardo.icasillas@gmail.com>
2023-06-13 11:26:14 -0600

    -Add json response -Add new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice!')
+
 fruityvice_response  = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json)
