a0081cb7e73f5c1c005a8e4bf7161121305431da
ribarra96 <ricardo.icasillas@gmail.com>
2023-06-13 13:35:59 -0600

    -binding instead of format

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,9 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
-my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (FRUIT_NAME), VALUES ({fruit})".format(add_my_fruit))
+my_cur.execute(
+    "INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (FRUIT_NAME) "
+    "VALUES (%s)", (
+        add_my_fruit
+    ))
 streamlit.write('Thanks for adding', add_my_fruit)
