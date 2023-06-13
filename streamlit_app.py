c8a9f312d6c9f68bc1408e639176e58e138d19ca
ribarra96 <ricardo.icasillas@gmail.com>
2023-06-13 11:23:29 -0600

    -add request library -request watermelon info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healty Diner')
 
@@ -17,4 +18,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
-
+fruityvice_response  = request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
