7ab28058b6e4c5278faf30a190e768529011be70
ribarra96 <ricardo.icasillas@gmail.com>
2023-06-13 13:38:20 -0600

    -Refresh table after insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ my_cur.execute(
         add_my_fruit
     ))
 streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
